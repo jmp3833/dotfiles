e3b9b6bfa39d691c2ea80ebec8735ec41256659a
Justin Peterson <jmp3833@rit.edu>
2015-07-16 17:00:15 +0000

    Setup github username in script

--- apply_configs.sh
+++ apply_configs.sh
@@ -1,3 +1,6 @@
+git config --global user.name "Justin Peterson"
+git config --global user.email jmp3833@rit.edu
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp bash_profile ~/.bash_profile
